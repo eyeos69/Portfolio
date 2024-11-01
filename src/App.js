1313359b6a06a1ca3be94258ef6555597e4dc041
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-02 06:22:43 +0800

    next commit

--- src/App.js
+++ src/App.js
@@ -7,32 +7,31 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Project1 from "./components/Project1";
 import Project2 from "./components/Project2";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className='body'>
-    <BrowserRouter> 
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              
-              <Hero />
-              <Skills />
-              <About />
-              <Work />
-              <Contact />
-              <Footer />
-            </div>
-          }
-        />
-        <Route path="/project1" element={<Project1 />} />
-        <Route path="/project2" element={<Project2 />} />
-      </Routes>
-    </BrowserRouter>
+      <Router> 
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Hero />
+                <Skills />
+                <About />
+                <Work />
+                <Contact />
+                <Footer />
+              </div>
+            }
+          />
+          <Route path="/project1" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
