2ddf738f179c3acd055393e22f81196eef475deb
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-04 04:22:31 +0800

    next commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -18,7 +18,7 @@ const Work = () => {
               <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
                 <span className='text-2x1 font-bold text-white tracking-wider'></span>
                 <div className='pt-8 text-center'>
-                  <Link to='/#project1'>
+                  <Link to='Portfolio/#project1'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Live
                     </button>
@@ -31,7 +31,7 @@ const Work = () => {
               <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
                 <span className='text-2x1 font-bold text-white tracking-wider'></span>
                 <div className='pt-8 text-center'>
-                  <Link to='/#project2'>
+                  <Link to='Portfolio/#project2'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Live
                     </button>
