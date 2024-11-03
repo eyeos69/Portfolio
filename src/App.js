a58111434f6b7b6076b49684139ad6f17df13a50
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-04 04:06:54 +0800

    next commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className='body'>
       
-      <BrowserRouter basename="/Portfolio">
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route
