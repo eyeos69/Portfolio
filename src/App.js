06663ac55a4326142329c107c4ba082cf044ec71
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-04 04:55:41 +0800

    next commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Project1 from "./components/Project1";
+import Project2 from "./components/Project2";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className='body'>
-      <BrowserRouter basename="/portfolio">
+      
+      <HashRouter basename="/Portfolio">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Hero />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Hero/>
+                <Skills/>
+                <About/>
+                <Work/>
+                <Contact/>
+                <Footer/>
+              </div>
+            }
+          />
+          <Route path="project1" element={<Project1/>} />
+          <Route path="project2" element={<Project2/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
