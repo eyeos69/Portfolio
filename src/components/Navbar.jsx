c86f821ec95e3a60da5a22ec84ceba45a2991ac4
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-05 08:23:05 +0800

    FINAL COMMIT NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { HashLink } from 'react-router-hash-link';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
-    const menuRef = useRef(null); // Reference for the menu div
 
     const handleNav = () => {
         setNav(!nav);
     };
 
-    // Close the menu if a click is detected outside of it
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
-                setNav(false);
-            }
-        };
-        // Attach the event listener when the menu is open
-        if (nav) {
-            document.addEventListener('mousedown', handleClickOutside);
-        }
-        return () => {
-            // Clean up the event listener when the menu is closed
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [nav]);
+    const closeNav = () => {
+        setNav(false);
+    };
 
     return (
         <div className='pt-10 pb-10 sm:pt-10 sm:pb-10 lg:pb-1'>
@@ -39,29 +25,37 @@ const Navbar = () => {
                         <li className='p-5'><HashLink smooth to="/">Home</HashLink></li>
                     </ul>
 
-                    {/* Menu icon for mobile view */}
+                    {/* Hamburger Icon */}
                     <div onClick={handleNav} className='block md:hidden mr-6'>
-                        <AiOutlineMenu size={20} />
+                        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                     </div>
+
+                    {/* Overlay */}
+                    {nav && (
+                        <div
+                            onClick={closeNav}
+                            className="fixed inset-0 bg-black/50 z-40"
+                        />
+                    )}
 
                     {/* Mobile Menu */}
                     <div
-                        ref={menuRef}
-                        className={`fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500 ${
-                            nav ? 'left-0' : 'left-[-100%]'
-                        }`}
+                        className={
+                            nav
+                                ? 'z-50 fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500 transform translate-x-0'
+                                : 'fixed left-0 top-0 w-[60%] h-full bg-[#202121] ease-in-out duration-500 transform -translate-x-full'
+                        }
                     >
-                        {/* Close icon inside the menu */}
-                        <div className="p-4 flex justify-end">
-                            <AiOutlineClose size={20} onClick={handleNav} className="cursor-pointer" />
+                        {/* Close Button inside the navbar */}
+                        <div onClick={handleNav} className="text-right p-4">
+                            <AiOutlineClose size={20} />
                         </div>
-
                         <h1 className='text-3xl font-bold primary-color ml-4'>Alvarez</h1>
                         <ul className='p-8 text-2xl'>
-                            <li className='p-5'><HashLink smooth to="/#about" onClick={handleNav}>About</HashLink></li>
-                            <li className='p-5'><HashLink smooth to="/#work" onClick={handleNav}>Work</HashLink></li>
-                            <li className='p-5'><HashLink smooth to="/#contact" onClick={handleNav}>Contact</HashLink></li>
-                            <li className='p-5'><HashLink smooth to="/" onClick={handleNav}>Home</HashLink></li>
+                            <li className='p-5'><HashLink smooth to="/#about" onClick={closeNav}>About</HashLink></li>
+                            <li className='p-5'><HashLink smooth to="/#work" onClick={closeNav}>Work</HashLink></li>
+                            <li className='p-5'><HashLink smooth to="/#contact" onClick={closeNav}>Contact</HashLink></li>
+                            <li className='p-5'><HashLink smooth to="/" onClick={closeNav}>Home</HashLink></li>
                         </ul>
                     </div>
                 </div>
